cce208287b69d5b45d11430504495fd50a4abe1d
daniel schulz <daniel.schulz@baqend.com>
2021-04-17 21:33:53 +0200

    add image index

---
+++ pages/CardsCollection.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<!-- nuxt img doesn't generate images from sub components yet -->
+		<!-- building index manually -->
+		<!-- this needs to be the same image as in <Card> -->
+		<nuxt-picture
+			v-for="(card, index) in cards"
+			:key="index"
+			format="webp"
+			:src="`/cards/card${index + 1}.jpg`"
+			sizes="s:190px l:240px"
+			:id="`img-${index + 1}`"
+		/>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from '@vue/composition-api'
+import { StoreInterface } from '~/storeObject/store'
+
+export default defineComponent({
+	setup() {
+		const store = inject('store') as StoreInterface
+		const cards = store.getCards()
+
+		return { cards }
+	},
+})
+</script>
